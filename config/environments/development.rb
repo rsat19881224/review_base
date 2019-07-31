b40ed3bf92c504f2da61ce33a35bba73c6d39ed1
RSat <rsat19881224@gmail.com>
2019-07-31 17:21:26 +0900

    devise install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,10 +30,22 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #default url
+  # mailer setting
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # mail setting RSat 20190731
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "rsat19881224@gmail.com",
+    :password => "mainichi8",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  # config.action_mailer.perform_caching = false
 
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
